75494bf7b3ea48006a96b8d17b25658993a23e1a
smugsby <dhuffman87@gmail.com>
2022-10-05 14:40:43 -0500

    first commit

---
+++ assets/script.js
@@ -0,0 +1,48 @@
+
+function test() {
+    //get distance input
+    var dist = document.getElementById("distance").value;
+    //get putt input
+    var putt = document.getElementById("puttsMade").value;
+    //get firstOne input
+    var first = document.getElementById("firstMade").checked;
+    if (first == true) {
+        first = Number(1)
+    } else {
+        first = Number(0)
+    }
+    //get lastOne input
+    var last = document.getElementById("lastMade").checked;
+    if (last == true) {
+        last = Number(1)
+    } else {
+        last = Number(0)
+    };
+    //get allMade input
+    var all = document.getElementById("allMade").checked;
+    if (all == true) {
+        all = Number(3)
+    } else {
+        all = Number(0)
+    };
+
+    //get feet multiplier
+    var feetMutli = (dist / 10);
+    //input total
+    var inputTotal = (parseInt(putt) + parseInt(first) + parseInt(last) + parseInt(all));
+    var totalScore = (feetMutli * inputTotal);
+    // alert("this is dist" + dist)
+    // alert("this is putt" + putt)
+    // alert("this is first" + first)
+    // alert("this is last" + last)
+    // alert("this is all" + all)
+    // alert("this is feetmulti" + feetMutli)
+    // alert("this is inputTotal" + inputTotal)
+    // alert("this is totalScore" + totalScore)
+    alert(totalScore);
+    var outputEl = document.getElementById("score");
+    outputEl.innerHTML = totalScore;
+    var scoreEl = document.getElementById("submit");
+    scoreEl.innerHTML = totalScore;
+};
+
